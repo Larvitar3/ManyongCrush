7b2fa80f975ae809e9c6c7853f2a8f0046eac979
whwlgns42 <whwlgns42@kakao.com>
2022-09-29 10:46:30 +0900

    메테오 구현 및 속도 조정

--- src/manyongCrush/MainFrame.java
+++ src/manyongCrush/MainFrame.java
@@ -2,6 +2,8 @@ package manyongCrush;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,6 +13,8 @@ public class MainFrame extends JFrame implements ActionListener {
 
 	JLabel backgroundHellImage;
 	JLabel backgroundNormalImage;
+
+	List<Meteor> meteorList;
 	int modeCount;
 
 	public MainFrame(int modeCount) {
@@ -24,36 +28,68 @@ public class MainFrame extends JFrame implements ActionListener {
 		setSize(1000, 700);
 		setTitle("ManyongCrush");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
+		if (modeCount == 1 || modeCount == 2) {
+			meteorList = new ArrayList<Meteor>();
+			for (int i = 0; i < 5; i++) {
+				meteorList.add(new Meteor());
+			}
+			meteorStart(modeCount);
+		} else {
+			meteorList = new ArrayList<Meteor>();
+			for (int i = 0; i < 10; i++) {
+				meteorList.add(new Meteor());
+			}
+			meteorStart(modeCount);
+		}
+
 		if (modeCount == 1) {
 			// 마법사 / 노말
 			backgroundNormalImage = new JLabel(new ImageIcon("images/bossBackgroundMap.jpg"));
 			setContentPane(backgroundNormalImage);
 			System.out.println("마법사 / 노말");
-			
 		} else if (modeCount == 2) {
 			// 전사 / 노말
 			backgroundNormalImage = new JLabel(new ImageIcon("images/bossBackgroundMap.jpg"));
 			setContentPane(backgroundNormalImage);
 			System.out.println("전사 / 노말");
-			
+
 		} else if (modeCount == 3) {
 			// 마법사 / 헬
 			backgroundHellImage = new JLabel(new ImageIcon("images/bossBackgroundMapHell.jpg"));
 			setContentPane(backgroundHellImage);
 			System.out.println("마법사 / 헬");
-			
+
 		} else if (modeCount == 4) {
 			// 전사 / 헬
 			backgroundHellImage = new JLabel(new ImageIcon("images/bossBackgroundMapHell.jpg"));
 			setContentPane(backgroundHellImage);
 			System.out.println("전사 / 헬");
-			
+
 		} else {
 			System.out.println("테스트용 메인 카운트값 확인 바람");
-		} 
+		}
 
+	}
 
+	private void meteorStart(int mode) {
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				for (int i = 0; i < meteorList.size(); i++) {
+					meteorList.get(i).down(mode);
+					try {
+						Thread.sleep(500);
+
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
 	}
 
 	private void setInitLayout() {
@@ -61,6 +97,10 @@ public class MainFrame extends JFrame implements ActionListener {
 		setLayout(null);
 		setResizable(false);
 		setLocationRelativeTo(null);
+		meteorList.forEach((m) -> {
+			System.out.println("메테오 추가");
+			add(m);
+		});
 
 	}
 
@@ -75,7 +115,7 @@ public class MainFrame extends JFrame implements ActionListener {
 
 	public static void main(String[] args) {
 		// 테스트용 메인창
-		new MainFrame(6);
+		new MainFrame(3);
 
 	}
 
--- src/manyongCrush/Meteor.java
+++ src/manyongCrush/Meteor.java
@@ -2,20 +2,110 @@ package manyongCrush;
 
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Meteor extends JLabel{
+public class Meteor extends JLabel {
+	private Random random = new Random();
 
-	
+	Player player;
+
 	private int x;
 	private int y;
-	
-	private boolean lavaMeteorFalling;// true : 떨어짐
-	private boolean hited; // 플레이어가 맞았을때
-	
-	private Random random = new Random();
-	private int lavaMeteors;
+	private int power = 20;
+
 	private final int LAVA_METEOR_WIDTH = 57;
 	private final int LAVA_METEOR_HEIGHT = 123;
-	
+
+	private boolean lavaMeteorFalling;// true : 떨어짐
+	private boolean hited; // 플레이어가 맞았을때
+
+	private int lavaMeteors;
+
+	private ImageIcon lavaMeteorImage;
+	private ImageIcon lavaBoomMeteorImage;
+	private ChoiceLevel choiceLevel;
+
+	public Meteor() {
+		System.out.println("메테오 클래스");
+		initData();
+		setInitLayout();
+	}
+
+	private void initData() {
+		lavaMeteorImage = new ImageIcon("images/meteor.png");
+		lavaBoomMeteorImage = new ImageIcon("images/boomMeteor2.png");
+		lavaMeteors = random.nextInt(700) + 200; // x좌표값 랜덤설정 x좌표 200부터시작
+	}
+
+	private void setInitLayout() {
+		x = lavaMeteors; // x값 랜덤으로 받아옴
+		y = -500; // 프레임 밖에서 떨어짐
+		setLocation(x, y);
+		setIcon(lavaMeteorImage);
+		setSize(LAVA_METEOR_WIDTH, LAVA_METEOR_HEIGHT);
+	}
+
+	public void down(int mode) {
+
+		new Thread(() -> {
+			int downSpeed = 2;
+			while (true) {
+//				System.out.println("메테오 떨어짐");
+				if (!lavaMeteorFalling || x >= -300) {
+					setIcon(lavaMeteorImage);
+					if (mode == 1 || mode == 2) {
+						downSpeed = 2;
+					} else {
+						downSpeed = 4;
+					}
+
+					y += downSpeed;
+					setLocation(x, y);
+//					attack(); 
+				}
+				try {
+					Thread.sleep(random.nextInt(8) + 5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (y >= 800) {
+					x = random.nextInt(700) + 30;
+					y = -200;
+					setLocation(x, y);
+					setIcon(lavaMeteorImage);
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
+	}
+
+//	public void attack() {
+//		if (!player.isBeAttacked()) {
+//
+//			if ((Math.abs(x - player.getX()) - 20 < 70
+//					&& Math.abs(y - player.getY() + 60) < 40)) {
+//				try {
+//					setSize(148, 125);
+//					setIcon(lavaBoomMeteorImage);
+//					Thread.sleep(500);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//				y = -200;
+//				setLocation(x, y);
+//				System.out.println("메테오가 터집니다.");
+//				player.setHp(player.getHp() - power);
+//				System.out.println(player.getHp());
+//				player.beAttacked();
+//			
+//			}
+//		}
+//	}
+
 }
