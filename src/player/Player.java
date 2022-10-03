ddf28bbe8d75818d4a1a4475d9ff1d7ca21e4d5e
whwlgns42 <whwlgns42@kakao.com>
2022-10-03 21:09:11 +0900

    최종 pull 코드 충돌 해결 완료

--- src/player/Player.java
+++ src/player/Player.java
@@ -18,295 +18,279 @@ import playerAttackSkill.SkillImpact;
 @Setter
 public class Player extends JLabel implements Attack, Moveable {
 
-	private Player player = this;
+   private Player player = this;
 
-	protected String name;
+   protected String name;
 
-	protected int hp;
-	protected int power;
-	protected int x;
-	protected int y;
-	protected int playerWidth;
-	protected int playerHeight;
-	protected int state = 0;
-	protected int skillCount;
+   protected int hp;
+   protected int power;
+   protected int x;
+   protected int y;
+   protected int playerWidth;
+   protected int playerHeight;
+   protected int state = 0;
+   protected int skillCount;
 
-	protected boolean left;
-	protected boolean right;
-	protected boolean jump;
-	protected boolean down;
+   protected boolean left;
+   protected boolean right;
+   protected boolean jump;
+   protected boolean down;
 
-	protected boolean crashWallL;
-	protected boolean crashWallR;
-	protected boolean crashBoss;
+   protected boolean crashWallL;
+   protected boolean crashWallR;
+   protected boolean crashBoss;
 
-	protected boolean beAttacked;
-	protected boolean attacking;
+   protected boolean beAttacked;
 
-	protected boolean attackCoolTime;
-	protected boolean skillCoolTime;
+   protected boolean attackCoolTime;
+   protected boolean skillCoolTime;
 
-	protected final int SPEED = 4;
-	protected final int JUMPSPEED = 2;
-	protected final int DOWNSPEED = 4;
+   protected final int SPEED = 4;
+   protected final int JUMPSPEED = 2;
+   protected final int DOWNSPEED = 4;
 
-	private final int BOSSCRASHDAMAGE = 10;
-	private final int BOTTOMFIREDAMAGE = 10;
+   private final int BOSSCRASHDAMAGE = 10;
+   private final int BOTTOMFIREDAMAGE = 10;
 
-	private int bossX;
-	private int bossY;
+   private int bossX;
+   private int bossY;
 
-	private int bossWidth;
-	private int bossHeight;
+   private int bossWidth;
+   private int bossHeight;
 
-	private int bressX;
-	private int bressY;
+   private int bressX;
+   private int bressY;
 
-	private int bressWidth;
-	private int bressHeight;
+   private int bressWidth;
+   private int bressHeight;
 
-	private int bressDamage;
+   private int bressDamage;
 
-	protected PlayerWay pWay;
+   protected PlayerWay pWay;
 
-	protected SkillImpact skillImpact;
+   protected SkillImpact skillImpact;
 
-	protected ImageIcon[] playerLeftAttackMotionImg = new ImageIcon[2];
-	protected ImageIcon[] playerRightAttackMotionImg = new ImageIcon[2];
+   protected ImageIcon[] playerLeftAttackMotionImg = new ImageIcon[2];
+   protected ImageIcon[] playerRightAttackMotionImg = new ImageIcon[2];
 
-	protected ImageIcon[] playerLeftSkillMotionImg = new ImageIcon[2];
-	protected ImageIcon[] playerRightSkillMotionImg = new ImageIcon[2];
+   protected ImageIcon[] playerLeftSkillMotionImg = new ImageIcon[2];
+   protected ImageIcon[] playerRightSkillMotionImg = new ImageIcon[2];
 
-	protected ImageIcon[] playerLeftBeAttacked = new ImageIcon[7];
-	protected ImageIcon[] playerRightBeAttacked = new ImageIcon[7];
+   protected ImageIcon[] playerLeftBeAttacked = new ImageIcon[7];
+   protected ImageIcon[] playerRightBeAttacked = new ImageIcon[7];
 
-	protected ImageIcon playerDieMotionImg;
+   protected ImageIcon playerDieMotionImg;
 
-	protected Ground groundContext;
+   protected Ground groundContext;
 
-	protected BottomFire bottomFire;
+   protected BottomFire bottomFire;
 
-	public Player(Ground groundContext, String name, int hp, int x, int y, int playerWidth, int playerHeight) {
-		this.name = name;
-		this.hp = hp;
-		this.x = x;
-		this.y = y;
-		this.playerWidth = playerWidth;
-		this.playerHeight = playerHeight;
-		this.groundContext = groundContext;
-		this.bottomFire = new BottomFire(this);
+   public Player(Ground groundContext, String name, int hp, int x, int y, int playerWidth, int playerHeight) {
+      this.name = name;
+      this.hp = hp;
+      this.x = x;
+      this.y = y;
+      this.playerWidth = playerWidth;
+      this.playerHeight = playerHeight;
+      this.groundContext = groundContext;
+      this.bottomFire = new BottomFire(this);
 
-		skillCount = 5;
-		down = false;
-	}
+      skillCount = 5;
+      down = false;
+   }
 
-	protected void setInitLayout() {
-		playerDieMotionImg = new ImageIcon("images/characterDie.png");
-		setIcon(playerLeftAttackMotionImg[1]);
-		setSize(playerWidth, playerHeight);
-		setLocation(x, y);
+   protected void setInitLayout() {
+      playerDieMotionImg = new ImageIcon("images/characterDie.png");
+      setIcon(playerLeftAttackMotionImg[1]);
+      setSize(playerWidth, playerHeight);
+      setLocation(x, y);
 
-		bossX = groundContext.getBoss().getX();
-		bossY = groundContext.getBoss().getY();
+      bossX = groundContext.getBoss().getX();
+      bossY = groundContext.getBoss().getY();
 
-		bossWidth = groundContext.getBoss().getWidth();
-		bossHeight = groundContext.getBoss().getHeight();
+      bossWidth = groundContext.getBoss().getWidth();
+      bossHeight = groundContext.getBoss().getHeight();
 
-	}
+   }
 
-	@Override
-	public void right() {
-		pWay = (PlayerWay.RIGHT); // 오른쪽으로 보고있을때
-		right = true;
-		new Thread(new Runnable() {
+   @Override
+   public void right() {
+      pWay = (PlayerWay.RIGHT); // 오른쪽으로 보고있을때
+      right = true;
+      new Thread(new Runnable() {
 
-			@Override
-			public void run() {
+         @Override
+         public void run() {
 
-				while (right) {
-					setIcon(playerRightAttackMotionImg[1]);
-					x += SPEED;
-					setLocation(x, y);
-					crashBoss();
-					bottomFireCheck();
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+            while (right) {
+               setIcon(playerRightAttackMotionImg[1]);
+               x += SPEED;
+               setLocation(x, y);
+               crashBoss();
+               bottomFireCheck();
+               try {
+                  Thread.sleep(10);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	@Override
-	public void left() {
-		pWay = PlayerWay.LEFT;
+   @Override
+   public void left() {
+      pWay = PlayerWay.LEFT;
 
-		left = true;
-		new Thread(new Runnable() {
+      left = true;
+      new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				while (left) {
-					setIcon(playerLeftAttackMotionImg[1]);
-					x -= SPEED;
-					setLocation(x, y);
-					bottomFireCheck();
-					crashBoss();
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+         @Override
+         public void run() {
+            while (left) {
+               setIcon(playerLeftAttackMotionImg[1]);
+               x -= SPEED;
+               setLocation(x, y);
+               bottomFireCheck();
+               crashBoss();
+               try {
+                  Thread.sleep(10);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	@Override
-	public void down() {
-		down = true;
+   @Override
+   public void down() {
+      down = true;
 
-		new Thread(new Runnable() {
+      new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				while (down) {
-					y += DOWNSPEED;
-					setLocation(x, y);
-					crashBoss();
-					bottomFireCheck();
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				down = false;
-			}
-		}).start();
-	}
+         @Override
+         public void run() {
+            while (down) {
+               y += DOWNSPEED;
+               setLocation(x, y);
+               crashBoss();
+               bottomFireCheck();
+               try {
+                  Thread.sleep(10);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+            down = false;
+         }
+      }).start();
+   }
 
-	@Override
-	public void jump() {
-		jump = true;
+   @Override
+   public void jump() {
+      jump = true;
 
-		new Thread(new Runnable() {
+      new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				for (int i = 0; i < 130 / JUMPSPEED; i++) {
-					y -= JUMPSPEED;
-					setLocation(x, y);
-					crashBoss();
-					bottomFireCheck();
-					try {
-						Thread.sleep(5);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				jump = false;
-				down();
-			}
-		}).start();
+         @Override
+         public void run() {
+            for (int i = 0; i < 130 / JUMPSPEED; i++) {
+               y -= JUMPSPEED;
+               setLocation(x, y);
+               crashBoss();
+               bottomFireCheck();
+               try {
+                  Thread.sleep(5);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+            jump = false;
+            down();
+         }
+      }).start();
 
-	}
+   }
 
-	@Override
-	public void die() {
-		
-		new Thread(() -> {
-			state = 1;
-			setIcon(playerDieMotionImg);
-			try {
-				Thread.sleep(2500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			new GameState(state - 1);
-			groundContext.setVisible(false);
-		}).start();
+   @Override
+   public void die() {
+      new Thread(() -> {
+         state = 1;
+         setIcon(playerDieMotionImg);
+         try {
+            Thread.sleep(2500);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+         new GameState(state - 1);
+         groundContext.setVisible(false);
+      }).start();
 
-	}
+   }
 
-	@Override
-	public void beAttacked(int damage) {
+   @Override
+   public void beAttacked(int damage) {
 
-		new Thread(() -> {
-<<<<<<< HEAD
-		
-			if (state == 0) {
-				beAttacked = true;
-=======
+      new Thread(() -> {
 
-			if (state == 0 && beAttacked) {
->>>>>>> 694479026e79b3cfe0abf7c8eaef007ddaf90a65
-				groundContext.unitHpInfo();
-				if (hp <= 0) {
-					hp = 0;
-					die();
-					return;
-				}
-				hp -= damage;
-				for (int i = 0; i < playerLeftBeAttacked.length; i++) {
-					try {
-						if (pWay == PlayerWay.LEFT) {
-							setIcon(playerLeftBeAttacked[i]);
-							x += 2;
-							y -= 2;
-							setLocation(x, y);
-						} else {
-							setIcon(playerRightBeAttacked[i]);
-							x -= 2;
-							y -= 2;
-							setLocation(x, y);
-						}
-						Thread.sleep(50);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			beAttacked = false;
-		}).start();
-	}
+         if (state == 0 && beAttacked) {
+            groundContext.unitHpInfo();
+            if (hp <= 0) {
+               hp = 0;
+               die();
+            }
+            hp -= damage;
+            for (int i = 0; i < playerLeftBeAttacked.length; i++) {
+               try {
+                  if (pWay == PlayerWay.LEFT) {
+                     setIcon(playerLeftBeAttacked[i]);
+                     x += 2;
+                     y -= 2;
+                     setLocation(x, y);
+                  } else {
+                     setIcon(playerRightBeAttacked[i]);
+                     x -= 2;
+                     y -= 2;
+                     setLocation(x, y);
+                  }
+                  Thread.sleep(50);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+         beAttacked = false;
+      }).start();
+   }
 
-	public void crashBoss() {
+   public void crashBoss() {
 
-		if ((Math.abs((x + (WIDTH / 2)) - (bossX + (bossWidth / 2))) < 200
-				&& Math.abs((y + (HEIGHT / 2)) - ((bossY + bossHeight) / 2)) < 250)) {
-			beAttacked = true;
-			try {
-				beAttacked(BOSSCRASHDAMAGE);
-				Thread.sleep(500);
-				System.out.println("먹나요");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		beAttacked = false;
-	}
+      if ((Math.abs((x + (WIDTH / 2)) - (bossX + (bossWidth / 2))) < 200
+            && Math.abs((y + (HEIGHT / 2)) - ((bossY + bossHeight) / 2)) < 250)) {
+         beAttacked = true;
+         try {
+            beAttacked(BOSSCRASHDAMAGE);
+            Thread.sleep(500);
+            System.out.println("먹나요");
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
+      beAttacked = false;
+   }
 
-	public void bottomFireCheck() {
+   public void bottomFireCheck() {
 
-<<<<<<< HEAD
-		
-		beAttacked = true;
-		
-		if (x > bottomFire.getX() && y > bottomFire.getY()) {
-=======
-		if (x + WIDTH > bottomFire.getX() && y + (HEIGHT / 2) > bottomFire.getY()) {
-			beAttacked = true;
->>>>>>> 694479026e79b3cfe0abf7c8eaef007ddaf90a65
-			try {
-				beAttacked(BOTTOMFIREDAMAGE);
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		beAttacked = false;
-	}
+      if (x + WIDTH > bottomFire.getX() && y + (HEIGHT / 2) > bottomFire.getY()) {
+         beAttacked = true;
+         try {
+            beAttacked(BOTTOMFIREDAMAGE);
+            Thread.sleep(500);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
+      beAttacked = false;
+   }
 }
